6ffa282ccd0370b962bcfe0aa564b84b2617709e
주태윤 <vividjtj@gmail.com>
2023-08-29 20:55:48 +0900

    첫번째 커밋

---
+++ src/main/java/hello/hellostpring/HelloStpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellostpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloStpringApplication.class, args);
+	}
+
+}
