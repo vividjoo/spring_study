9d2619f2c2cae88819bdf6c6bd2a8ab664d02f3c
주태윤 <vividjtj@gmail.com>
2023-09-03 22:00:31 +0900

    두번째 커밋

---
+++ src/test/java/hello/hellostpring/HelloStpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellostpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloStpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
